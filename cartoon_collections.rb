22c51a965c7ab86a423a296064c7e8e8f75a0a82
Ryan Pfeifer <pfeifer.ryan@gmail.com>
2017-11-07 06:08:08 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
 def roll_call_dwarves(dwarves)
-  dwarves.each_with_index {|dwarf, index| puts "#{index}. *#{dwarf}"}
+  puts dwarves.each_with_index {|dwarf, index| "#{index}. *#{dwarf}"}
 end
 
 def summon_captain_planet(elements)
