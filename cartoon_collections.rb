196d2cc7fac4573d6ee49953de062740ec381387
Ryan Pfeifer <pfeifer.ryan@gmail.com>
2017-11-07 06:05:28 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
 def roll_call_dwarves(dwarves)
-  dwarves.each_with_index {|dwarfm index| puts "/#{index}. *#{dwarf}/"
+  dwarves.each_with_index {|dwarfm index| puts "/#{index}. *#{dwarf}/"}
 end
 
 def summon_captain_planet(elements)
@@ -10,7 +10,7 @@ def long_planeteer_calls(calls)
   !!calls.find {|call| call.size > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(foods)
   cheese_types = ["cheddar", "gouda", "camembert"]
-end
+  
+en
